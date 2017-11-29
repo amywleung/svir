d0c09325cf9b7a98e1cfb581342ec949403c6401
Maximilian Pschorr <mpschorr@clarku.edu>
2017-11-29 01:33:59 -0500

    Changed order of checkbox column headings for tabular tab

--- R/ui.R
+++ R/ui.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(leaflet)
 library(shinythemes)
-library(DT)
 
 # Set limit for file upload
 options(shiny.maxRequestSize = 50 * 1024 ^ 2)
@@ -46,13 +45,13 @@ navbarPage(theme = shinytheme("slate"),
              sidebarLayout(
                sidebarPanel(
                  checkboxGroupInput("show_vars", "Columns in SVI:",
-                                    selected = c('rpl_theme1', 'rpl_theme2', 'rpl_theme3',
-                                                 'rpl_theme4', 'rpl_themes', "f_pov", "f_unemp",
+                                    selected = c('rpl_themes', 'rpl_theme1', 'rpl_theme2', 'rpl_theme3',
+                                                 'rpl_theme4', "f_pov", "f_unemp",
                                                  "f_pci", "f_nohsdp", "f_age65", "f_age17",
                                                  "f_disabl", "f_sngpnt", "f_minrty", "f_limeng",
                                                  "f_munit", "f_mobile", "f_crowd", "f_noveh", "f_groupq"),
-                                    c('rpl_theme1', 'rpl_theme2', 'rpl_theme3',
-                                      'rpl_theme4', 'rpl_themes', "f_pov", "f_unemp",
+                                    c('rpl_themes', 'rpl_theme1', 'rpl_theme2', 'rpl_theme3',
+                                      'rpl_theme4', "f_pov", "f_unemp",
                                       "f_pci", "f_nohsdp", "f_age65", "f_age17",
                                       "f_disabl", "f_sngpnt", "f_minrty", "f_limeng",
                                       "f_munit", "f_mobile", "f_crowd", "f_noveh", "f_groupq")
