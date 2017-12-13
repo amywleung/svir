a24925c5e5e6637fae09ef630f2f15ea5fc29baa
Amy Leung <amy.wl.leung@gmail.com>
2017-12-12 22:11:05 -0500

    Added function documentation

--- R/makeDash.R
+++ R/makeDash.R
@@ -1,4 +1,22 @@
-makeDash <- function(shp, rpl, uid) {
+#' makeDash
+#'
+#' Creates and styles plotly dashboard using leaflet user click events. This
+#' dashboard visualizes each of the indicator variables that comprise a thematic
+#' domain, and updates dynamically based on click events.
+#'
+#' @param shp The SVI shapefile from the CDC website
+#' @param uid The click event polygon id
+#'
+#' @return A styled dashboard using the SVI shapefile \code{shp}
+#' and click event polygon id \code{uid}
+#'
+#' @examples
+#' makeDash(shp = uploadShpFile(), uid = 62878)
+#' makeDash(shp = uploadShpFile(), uid = event$id)
+#'
+#' @export
+
+makeDash <- function(shp, uid) {
   dat <- slot(shp, "data")
   t1 <- c("POV", "UNEMP", "PCI", "NOHSDP")
   t2 <- c("AGE65", "AGE17", "DISABL", "SNGPNT")
@@ -6,31 +24,31 @@ makeDash <- function(shp, rpl, uid) {
   t4 <- c("MUNIT", "MOBILE", "CROWD", "NOVEH", "GROUPQ")
   t1vals <-
     c(dat[["epl_pov"]][(dat$gid == uid) &
-                         (!dat[[rpl]] == -999)], dat[["epl_unemp"]][(dat$gid == uid) &
-                                                                      (!dat[[rpl]] == -999)],
+                         (!dat[["rpl_themes"]] == -999)], dat[["epl_unemp"]][(dat$gid == uid) &
+                                                                               (!dat[["rpl_themes"]] == -999)],
       dat[["epl_pci"]][(dat$gid == uid) &
-                         (!dat[[rpl]] == -999)], dat[["epl_nohsdp"]][(dat$gid == uid) &
-                                                                       (!dat[[rpl]] == -999)])
+                         (!dat[["rpl_themes"]] == -999)], dat[["epl_nohsdp"]][(dat$gid == uid) &
+                                                                                (!dat[["rpl_themes"]] == -999)])
   t2vals <-
     c(dat[["epl_age65"]][(dat$gid == uid) &
-                           (!dat[[rpl]] == -999)], dat[["epl_age17"]][(dat$gid == uid) &
-                                                                        (!dat[[rpl]] == -999)],
+                           (!dat[["rpl_themes"]] == -999)], dat[["epl_age17"]][(dat$gid == uid) &
+                                                                                 (!dat[["rpl_themes"]] == -999)],
       dat[["epl_disabl"]][(dat$gid == uid) &
-                            (!dat[[rpl]] == -999)], dat[["epl_sngpnt"]][(dat$gid == uid) &
-                                                                          (!dat[[rpl]] == -999)])
+                            (!dat[["rpl_themes"]] == -999)], dat[["epl_sngpnt"]][(dat$gid == uid) &
+                                                                                   (!dat[["rpl_themes"]] == -999)])
   t3vals <-
     c(dat[["epl_minrty"]][(dat$gid == uid) &
-                            (!dat[[rpl]] == -999)], dat[["epl_limeng"]][(dat$gid == uid) &
-                                                                          (!dat[[rpl]] == -999)])
+                            (!dat[["rpl_themes"]] == -999)], dat[["epl_limeng"]][(dat$gid == uid) &
+                                                                                   (!dat[["rpl_themes"]] == -999)])
   t4vals <-
     c(dat[["epl_munit"]][(dat$gid == uid) &
-                           (!dat[[rpl]] == -999)], dat[["epl_mobile"]][(dat$gid == uid) &
-                                                                         (!dat[[rpl]] == -999)],
+                           (!dat[["rpl_themes"]] == -999)], dat[["epl_mobile"]][(dat$gid == uid) &
+                                                                                  (!dat[["rpl_themes"]] == -999)],
       dat[["epl_crowd"]][(dat$gid == uid) &
-                           (!dat[[rpl]] == -999)], dat[["epl_noveh"]][(dat$gid == uid) &
-                                                                        (!dat[[rpl]] == -999)],
+                           (!dat[["rpl_themes"]] == -999)], dat[["epl_noveh"]][(dat$gid == uid) &
+                                                                                 (!dat[["rpl_themes"]] == -999)],
       dat[["epl_groupq"]][(dat$gid == uid) &
-                            (!dat[[rpl]] == -999)])
+                            (!dat[["rpl_themes"]] == -999)])
   p1 <-
     plot_ly(
       dat,
@@ -75,5 +93,4 @@ makeDash <- function(shp, rpl, uid) {
     ) %>%
     layout(xaxis = list(tickangle = 45))
   subp <- subplot(p1, p2, p3, p4, shareY = TRUE)
-  output$dash <- renderPlotly(subp)
 }
